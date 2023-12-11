653cf795a009aa28982a692b965a96f7c357324e
angarag <anguu0622lq7@gmail.com>
2023-12-11 11:46:51 +0800

    click

--- blog/src/components/Blog.jsx
+++ blog/src/components/Blog.jsx
@@ -1,46 +1,90 @@
-const BlogData = [
-  {
-    url: "",
-    buttn: "",
-    content: "",
-    date: "",
-  },
-];
+import axios from "axios";
+
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+
+const api = "https://dev.to/api/articles";
 
 export default function Blog() {
+  const [data, setData] = useState([]);
+  const valueRef = useRef("");
+  const initData = useRef([]);
+  const router = useRouter();
+  const getData = async (api) => {
+    let res = await axios.get(api);
+    initData.current = res.data;
+    setData(res.data);
+  };
+
+  const reset = () => setData(initData.current);
+
+  const filter = (name) =>
+    setData(() => initData.current.filter((el) => el.tags === name));
+
+  const [Count, setCount] = useState(6);
+
+  const handler = () => {
+    setCount((prev) => prev + 3);
+  };
+
+  const handlerRouter = (id) => router.push(`id=${id}`);
+  useEffect(() => {
+    getData(api);
+  }, []);
+
   return (
     <>
-      <div className="flex flex-col gap-[32px] px-[350px]">
-        <h1 className="text-[24px] text-[#181A2A]">All Blog Post</h1>
-        <div className="flex justify-between">
-          <div className="text-[#495057] text-[12px] flex gap-[20px]">
-            <p className="text-[#D4A373]">All</p>
-            <p>Design</p>
-            <p>Travel</p>
-            <p>Fashion</p>
-            <p>Technology</p>
-            <p>Branding</p>
-          </div>
-          <p className="text-[#495057] text-[12px] flex gap-[20px]">View All</p>
-        </div>
-        {BlogData.map((e) => {
-          return (
-            <div className="flex gap-[20px]">
-              <div className="gap-[16px] flex flex-col border px-[16px] py-[16px] rounded-xl ">
-                <img className="w-[360px] h-[240px]" src={e.url} alt="" />
-                <div>
-                  <button className="px-[10px] py-[4px] bg-indigo-100 rounded-[6px] text-[#4B6BFB]">
-                    {e.buttn}
-                  </button>
-                </div>
-                <div className="gap-[20px] flex flex-col">
-                  <h1 className="text-[24px] text-[#181A2A]">{e.content}</h1>
-                  <p className="text-[#97989F]">{e.date}</p>
-                </div>
-              </div>
+      <div className="flex">
+        <div className="flex flex-col gap-[32px] px-[350px]">
+          <h1 className="text-[24px] text-[#181A2A]">All Blog Post</h1>
+          <div className="flex justify-between">
+            <div className="text-[#030404] text-[12px] flex gap-[20px]">
+              <button onClick={reset} className="text-[#D4A373]">
+                All
+              </button>
+              <button onClick={() => filter("webdev")}>webdev</button>
+              <button onClick={() => filter("nextjs")}>Nextjs</button>
+              <button onClick={() => filter("programming")}>Programming</button>
+              <button onClick={() => filter("Technology")}>Technology</button>
+              <button onClick={() => filter("Branding")}>Branding</button>
             </div>
-          );
-        })}
+            <button
+              onClick={reset}
+              className="text-[#495057] text-[12px] flex gap-[20px]"
+            >
+              View All
+            </button>
+          </div>
+          <div className="flex gap-[20px] flex-wrap justify-center">
+            {data.slice(0, Count).map((el) => {
+              return (
+                <div className="flex gap-[20px] w-[392px]">
+                  <div className="gap-[16px] flex flex-col border px-[16px] py-[16px] rounded-xl ">
+                    <img
+                      className="w-[360px] h-[240px]"
+                      src={el.social_image}
+                      alt=""
+                    />
+                    <div>
+                      <button className="px-[10px] py-[4px] bg-indigo-100 rounded-[6px] text-[#4B6BFB]">
+                        {el.tag_list}
+                      </button>
+                    </div>
+                    <div className="gap-[20px] flex flex-col">
+                      <h1 className="text-[24px] text-[#181A2A]">
+                        {el.description}
+                      </h1>
+                      <p className="text-[#97989F]">
+                        {el.readable_publish_date}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <button onClick={handler}>Load more</button>
+        </div>
       </div>
     </>
   );
