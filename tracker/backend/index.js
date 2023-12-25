e19f50e37a29d1bc6ddbcbd1206cd0da9a81a1aa
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2023-12-25 12:39:18 +0800

    a

---
+++ tracker/backend/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv"
+import postgres from "postgres"
+
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+  },
+});
+
+async function getPostgresVersion() {
+  const client = await pool.connect();
+  try {
+    const response = await client.query('SELECT version()');
+    console.log(response.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+
+getPostgresVersion();
