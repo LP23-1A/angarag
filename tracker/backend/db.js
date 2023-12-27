10798a65fa3a98ac8dfbcaf2c800fb112c1ffcd3
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2023-12-27 12:26:48 +0800

    dasf

--- tracker/backend/db.js
+++ tracker/backend/db.js
@@ -2,22 +2,27 @@ import pg from "pg";
 import dotenv from "dotenv"
 import { text } from "express";
 
-const { Pool } = pg
+const { Pool } = pg;
 
 dotenv.config();
 
-console.log(process.env.DB_URL);
-
 export const pool = new Pool({
-    ssl:true,
-    connectionString:process.env.DB_URL
+    ssl: true,
+    connectionString: process.env.DB_URL
 })
 
-pool.connect();
-pool.query()
+// console.log(process.env.DB_URL);
 
-console.log(pool);
+// export const pool = new Pool({
+//     ssl:true,
+//     connectionString:process.env.DB_URL
+// })
 
-module.exports = {
-  query: (text, params) => pool.query(text,params)
-} 
+// pool.connect();
+// pool.query()
+
+// console.log(pool);
+
+// module.exports = {
+//   query: (text, params) => pool.query(text,params)
+// }  
