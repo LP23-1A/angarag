137a0dedf70888bd6e519e4b684ecc65351bbc4a
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2023-12-28 12:16:28 +0800

    fdsg

---
+++ tracker/frontEnd/src/app/components/Login.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+      <div className="">
+        <div></div>
+        <div></div>
+        <button className="btn btn-info bg-red-600">test</button>
+      </div>
+    )
+  }
